8b17710e02634aa6ce2eb0b2e720a8582f3002db
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-09-10 18:49:59 +0300

    Minor fix

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-// import { Typography } from "@mui/material";
-// import PageContainer from "../components/shared/page-container/PageContainer"
 import RegisterContainer from "../components/shared/register-container/RegisterContainer";
 
 function Tasks() {
