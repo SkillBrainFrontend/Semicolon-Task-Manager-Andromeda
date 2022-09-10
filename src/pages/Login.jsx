6fb86dd512c2ad7f9ceb80d39684ad7c6edacea7
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-09-10 23:48:24 +0300

    updated layout 2.2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,115 @@
 import React from "react";
-import { Box, Grid, Container, Typography } from "@mui/material";
+import {
+	Box,
+	Grid,
+	Avatar,
+	Typography,
+	TextField,
+	FormControlLabel,
+	Link,
+	Checkbox,
+	Button,
+	Paper,
+} from "@mui/material";
+
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 function LoginPage() {
 	return (
-		<Box sx={{ flexGrow: 1, border: "1px solid red" }}>
+		<Grid container component="main" sx={{ height: "100vh" }}>
 			<Grid
+				item
+				xs={false}
+				sm={4}
+				md={6}
 				justifyContent="center"
-				alignItems="stretch"
-				container
-				spacing={0}
-				columns={12}
-				sx={{ flexGrow: 1 }}
+				alignContent="stretch"
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
 			>
-				<Grid xs={6}>
-					<Container>
-						<Typography>Placeholder</Typography>
-					</Container>
-				</Grid>
-
-				<Grid xs={6}>
-					<Container>
-						<Typography>Login Form</Typography>
-					</Container>
-				</Grid>
+				<Box
+					sx={{
+						flex: 1,
+						height: "98%",
+						backgroundColor: "#3754DB",
+						borderRadius: "32px",
+					}}
+				/>
 			</Grid>
-		</Box>
+			<Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+				<Box
+					sx={{
+						my: 8,
+						mx: 4,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box
+						component="form"
+						noValidate
+						// onSubmit={handleSubmit}
+						sx={{ mt: 1 }}
+					>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						<FormControlLabel
+							control={<Checkbox value="remember" color="primary" />}
+							label="Remember me"
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="/test" variant="body2">
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href="/test" variant="body2">
+									Don&apos;t have an account? Sign Up
+								</Link>
+							</Grid>
+						</Grid>
+						{/* <Copyright sx={{ mt: 5 }} /> */}
+					</Box>
+				</Box>
+			</Grid>
+		</Grid>
 	);
 }
 
