5a5d66b4c1a1c9a1214a5c669d5b22bf48767107
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-09-17 21:17:30 +0300

    progresez

--- src/components/shared/alert/Alert.jsx
+++ src/components/shared/alert/Alert.jsx
@@ -1,61 +1,64 @@
 import * as React from "react";
-import MuiAlert from "@mui/material/Alert";
+
 import { Typography } from "@mui/material";
-import x from "./x.png";
-import "./App.css";
+import { MuiAlert } from "@mui/material/Alert";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
-const Alert = React.forwardRef(function Alert(props, ref) {
-	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
+import x from "./x.png";
+
+import "./Alert.css";
+
+const Alert = React.forwardRef((props, ref) => (
+	<MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
 
 function Alerta() {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
 			<Grid
+				borderRadius="12px"
 				container
+				direction="row"
+				display="flex"
+				justifyContent="space-between"
+				margin="5px 20px"
 				spacing={2}
 				sx={{ width: "96%" }}
-				display={"flex"}
-				direction={"row"}
-				justifyContent={"space-between"}
-				margin={"5px 20px"}
-				borderRadius={"12px"}
 			>
-				<Grid item xs={12} sm={6} md={6} lg={3}>
+				<Grid item lg={3} md={6} sm={6} xs={12}>
 					<Alert
+						color="info"
+						justify="center"
 						severity="warning"
-						color={"info"}
-						justify={"center"}
 						sx={{ borderRadius: "12px" }}
 					>
 						<Typography>
 							<p>Info Message</p>
 							<p>Some message</p>
 						</Typography>
-						<img src={x} alt="" />
+						<img alt="" src={x} />
 					</Alert>
 				</Grid>
 
-				<Grid item xs={12} sm={6} md={6} lg={3}>
+				<Grid item lg={3} md={6} sm={6} xs={12}>
 					<Alert
+						color="success"
 						severity="warning"
-						color={"success"}
 						sx={{ borderRadius: "12px" }}
 					>
 						<Typography>
 							<p>Succes Message</p>
 							<p>Some message</p>
 						</Typography>
-						<img src={x} alt="" />
+						<img alt="" src={x} />
 					</Alert>
 				</Grid>
 
-				<Grid item xs={12} sm={6} md={6} lg={3}>
+				<Grid item lg={3} md={6} sm={6} xs={12}>
 					<Alert
+						color="warning"
 						severity="warning"
-						color={"warning"}
 						sx={{ borderRadius: "12px" }}
 					>
 						<Typography>
@@ -63,21 +66,17 @@ function Alerta() {
 							<p>Some message</p>
 						</Typography>
 
-						<img src={x} alt="" />
+						<img alt="" src={x} />
 					</Alert>
 				</Grid>
 
-				<Grid item xs={12} sm={6} md={6} lg={3}>
-					<Alert
-						severity="warning"
-						color={"error"}
-						sx={{ borderRadius: "12px" }}
-					>
+				<Grid item lg={3} md={6} sm={6} xs={12}>
+					<Alert color="error" severity="warning" sx={{ borderRadius: "12px" }}>
 						<Typography>
 							<p>Error Message</p>
 							<p>Some message</p>
 						</Typography>
-						<img src={x} alt="" />
+						<img alt="" src={x} />
 					</Alert>
 				</Grid>
 			</Grid>
