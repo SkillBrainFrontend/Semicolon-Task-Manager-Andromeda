30520ba5168e0ea8431f3bfa9b20c0d463c060ad
Creola Tudorache <tudorachecreola@gmail.com>
2022-09-24 17:41:42 +0300

    forgot pass

--- src/components/shared/forgotpass/ForgotPass.jsx
+++ src/components/shared/forgotpass/ForgotPass.jsx
@@ -1,45 +1,52 @@
-import React from "react"
-import { Container, Box, FormControl, InputLabel, Alert } from '@mui/material';
+import React from "react";
+
+import { Stack, Typography, Button, Container } from "@mui/material";
 
 export default function ForgotPass() {
-    const EmailInput = styled(InputBase)(({ theme }) => ({
-        'label + &': {
-          marginTop: theme.spacing(3),
-        },
-        '& .MuiInputBase-input': {
-          borderRadius: 4,
-          position: 'relative',
-          backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
-          border: '1px solid #ced4da',
-          fontSize: 16,
-          width: 'auto',
-          padding: '10px 12px',
-          transition: theme.transitions.create([
-            'border-color',
-            'background-color',
-            'box-shadow',
-          ]),
-        }
-    return (
-    <div> 
-        <Container sx={{ display: 'flex', flexWrap: 'wrap' }}>
-            <Typography gutterBottom variant="h5">
-                Forgot Password?
-            </Typography>
-            <Typography gutterBottom variant="body1">
-                We are sorry to hear that happen. Don't be sad, we could help you get back to your productivity
-            </Typography>
-            <FormControl variant="standard">
-                <InputLabel shrink htmlFor="email-input">
-                    Email
-                </InputLabel>
-                <EmailInput  id="email-input" />
-            </FormControl>
-            < Button variant="contained" size="large">
-                Next
-            </Button>
-        </Container>
-        <Alert></Alert>
-    </div>
-    );
-  }
+	return (
+		<Container sx={{ display: "flex", flexWrap: "wrap" }}>
+			<Stack>
+				<Typography gutterBottom variant="h5">
+					Forgot Password?
+				</Typography>
+				<Typography gutterBottom variant="body1">
+					We are sorry to hear that happen. Don't be sad, we could help you get
+					back to your productivity
+				</Typography>
+			</Stack>
+			<Stack>
+				<Typography></Typography>
+				<Input
+					autoComplete="email"
+					autoFocus
+					error={errors.email}
+					fullWidth
+					helperText={errors.email}
+					id="email"
+					label="Email Address"
+					margin="normal"
+					name="email"
+					required
+				/>
+			</Stack>
+			<Stack>
+				<Button size="medium" variant="contained">
+					Next
+				</Button>
+			</Stack>
+			<Stack>
+				<Alert
+					onClose={() => {}}
+					severity="warning"
+					variant="outline"
+					sx={{
+						width: 350,
+					}}
+				>
+					<AlertTitle>HalloMano!</AlertTitle>
+					You will be sent a recovery link to your email!
+				</Alert>
+			</Stack>
+		</Container>
+	);
+}
