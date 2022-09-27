44f4a13f27e95d8a6706233eac2431fd40078f85
Flor77 <budaflorin@yahoo.com>
2022-09-27 20:22:06 +0300

    first commit

---
+++ src/pages/view-task-card/ViewTaskCardPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Grid } from "@mui/material";
+
+import { ViewTaskCard } from "../../components/shared";
+
+function ViewTaskCardPage() {
+	return (
+		<Grid component="main" container sx={{ height: "100vh" }}>
+			<Grid>
+				<ViewTaskCard />
+			</Grid>
+		</Grid>
+	);
+}
+
+export default ViewTaskCardPage;
