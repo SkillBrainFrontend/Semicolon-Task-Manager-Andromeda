9557e9607118513b1664763399f807c3af36d537
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-09-30 22:43:45 +0300

    minor fix

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import ModalContentWrapper from "./modal-content-wrapper/ModalContentWrapper";
 import { withNavigationDrawer } from "./navigation-drawer/withNavigationDrawer";
 import SidebarRight from "./sidebar-right/SidebarRight";
 import TaskCard from "./task-card/TaskCard";
+import ViewTaskCard from "./view-task-card/ViewTaskCard";
 
 export {
 	CreateTaskForm,
 	ModalContentWrapper,
 	SidebarRight,
 	TaskCard,
+	ViewTaskCard,
 	withNavigationDrawer,
 };
