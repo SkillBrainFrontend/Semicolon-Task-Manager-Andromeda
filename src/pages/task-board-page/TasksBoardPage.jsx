bf6383f1e00cc0a7a6f0d3f8fc701f017f5dad79
irazvanbotezatu <rezzy_botez@yahoo.com>
2022-10-05 20:27:14 +0300

    testing viewtaskcard page

--- src/pages/task-board-page/TasksBoardPage.jsx
+++ src/pages/task-board-page/TasksBoardPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import { withNavigationDrawer } from "../../components/navigation-drawer/withNavigationDrawer";
 import { Button } from "../../components/shared";
