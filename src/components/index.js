87fde6067a17f80c004ba7f8a5a845e113fe3224
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-10-10 20:43:05 +0300

    fixed bug

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-import Calendar from "./calendar/Calendar";
 import CreateTaskForm from "./create-task-form/CreateTaskForm";
 import Modal from "./modal-content-wrapper/ModalContentWrapper";
 import { withNavigationDrawer } from "./navigation-drawer/withNavigationDrawer";
@@ -7,7 +6,6 @@ import TaskCard from "./task-card/TaskCard";
 import ViewTaskCard from "./view-task-card/ViewTaskCard";
 
 export {
-	Calendar,
 	CreateTaskForm,
 	Modal,
 	SidebarRight,
